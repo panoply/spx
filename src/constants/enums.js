cba95a96abc51b1e52d480127465184ecbc710ac
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-03-09 01:29:29 +0100

    create enums for fetch

---
+++ src/constants/enums.js
@@ -0,0 +1,24 @@
+/**
+ * XMLHttp Request fetch was successful
+ */
+export const XHRSuccess = 1
+
+/**
+ * XMLHttp Request fetch to url is in flight
+ */
+export const XHRExists = 2
+
+/**
+ * XMLHttp Request was prevented from dispatched event
+ */
+export const XHRPrevented = 3
+
+/**
+ * XMLHttp Request was prevented from dispatched event
+ */
+export const XHREmpty = 4
+
+/**
+ * XMLHttp Request failed to fetch
+ */
+export const XHRFailed = 5
