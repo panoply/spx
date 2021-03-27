039acd61a6470c51f55ec3321beb7b171f96aa74
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-03-27 01:17:31 +0100

    various changes and updates.

--- src/app/store.js
+++ src/app/store.js
@@ -1,29 +1,14 @@
 import merge from 'mergerino'
 import history from 'history/browser'
-import { dispatchEvent } from './utils'
 import { nanoid } from 'nanoid'
-import path from './path'
+import { dispatchEvent } from './utils'
 import scroll from '../observers/scroll'
 import * as nprogress from './progress'
 
 /**
- * Snapshots Cache
- *
- * @exports
- */
-export const snapshots = new Map()
-
-/**
  * store
  */
-export const store = (() => {
-
-  /**
-   * Preset Configuration
-   *
-   * @type {object}
-   */
-  let presets
+export default ((config) => {
 
   /**
    * Cache
@@ -33,7 +18,20 @@ export const store = (() => {
    */
   const cache = new Map()
 
-  /* CLOSURE IIFE ------------------------------- */
+  /**
+   * Cache
+   *
+   * @exports
+   * @type {Map<string, string>}
+   */
+  const snapshots = new Map()
+
+  /**
+   * Preset Configuration
+   *
+   * @type {object}
+   */
+  let presets
 
   return ({
 
@@ -47,89 +45,14 @@ export const store = (() => {
      */
     connect (options = {}) {
 
-      presets = merge(
-        {
-          targets: [ 'main', '#navbar' ],
-          request: {
-            timeout: 1500,
-            throttle: 150,
-            dispatch: 'mousedown'
-          },
-          prefetch: {
-            mouseover: {
-              enable: true,
-              threshold: 100
-            },
-            intersect: {
-              enable: true,
-              threshold: 250
-            }
-          },
-          cache: {
-            enable: true,
-            limit: 25,
-            save: false
-          },
-          progress: {
-            enable: true,
-            threshold: 350,
-            options: {
-              minimum: 0.10,
-              easing: 'ease',
-              speed: 225,
-              trickle: true,
-              trickleSpeed: 225,
-              showSpinner: false
-            }
-          }
-        }
-        ,
-        {
-
-          // PRESETS PATCH COPY
-
-          ...options
-
-          , request: {
-
-            ...options?.request
-
-            // PREVENT DISPATCH PROPERTY FROM PATCH
-
-            , dispatch: undefined
-          }
-          , cache: {
-
-            ...options?.cache
-
-            // PREVENT SAVE PROPERTY FROM PATCH
-
-            , save: undefined
-          }
-        }
-      )
+      presets = merge(config, {
+        // PRESETS PATCH COPY
+        ...options
+        , request: { ...options?.request, dispatch: undefined }
+        , cache: { ...options?.cache, save: undefined }
+      })
 
       nprogress.config(this.config.progress.options)
-
-    },
-
-    /**
-     * Sets initial page state executing on intial load.
-     * Caches page so a return navigation does not perform
-     * an extrenous request.
-     *
-     * @param {Event} event
-     * @returns {void}
-     */
-    initialize (event) {
-
-      history.replace(history.location, store.create({
-        url: path.url,
-        location: path.parse(path.url),
-        position: scroll.position
-      }, document.documentElement.outerHTML))
-
-      removeEventListener('load', this.initialize)
 
     },
 
@@ -141,99 +64,7 @@ export const store = (() => {
      * @type {Store.IPresets}
      * @return {Store.IPresets}
      */
-    get config () {
-
-      return presets
-
-    },
-
-    clear: () => {
-
-      cache.clear()
-      snapshots.clear()
-
-    },
-
-    /**
-     * Check if cache record exists with snapshot
-     *
-     * @param {string} url
-     * @param {{snapshot?: boolean}} has
-     * @return {boolean}
-     */
-    has: (url, has = { snapshot: false }) => {
-
-      return !has.snapshot ? cache.has(url) : (
-        cache.has(url) || snapshots.has(cache.get(url)?.snapshot)
-      )
-
-    },
-
-    /**
-     * Saves a HTML Document String as snapshot
-     *
-     * @param {string} id
-     * @returns {string}
-     */
-    snapshot: (id) => {
-
-      return snapshots.get(id)
-
-    },
-
-    /**
-     * Inserts a page into cache. This function is called upon
-     * a new page instance being generated via `new()`. Optionally
-     * pass in an `object`to assert into new state.
-     *
-     * @param {string} url
-     * @returns {Store.IPage}
-     */
-    cache (url) {
-
-      return cache.get(url)
-
-    },
-
-    /**
-     * Update current pushState History
-     *
-     * @param {string} url
-     * @returns {void}
-     */
-    history (url) {
-
-      history.replace(history.location, {
-        ...history.location.state,
-        position: scroll.position
-      })
-
-    },
-
-    /**
-     * Updates page state, this function will run a merge
-     * on the current page instance and re-assign the `pageState`
-     * letting to updated config.
-     *
-     * If newState contains a different `ILocation.url` value from
-     * that of the current page instance `url` then it will be updated
-     * to match that of the `newState.url` value.
-     *
-     * The cache will e updated accordingly, so `this.page` will provide
-     * access to the updated instance.
-     *
-     * @param {Store.IPage} state
-     * @returns {Store.IPage}
-     */
-    update: (state) => {
-
-      const page = cache
-        .set(state.url, merge(cache.get(state.url), state))
-        .get(state.url)
-
-      return page
-
-    },
+    get config () { return presets },
 
     /**
      * Indicates a new page visit or a return page visit. New visits
@@ -254,26 +85,220 @@ export const store = (() => {
     create (state, snapshot) {
 
       const page = {
-        captured: false,
-        append: false,
-        prepend: false,
+
+        // EDITABLE
+        //
+        history: true,
+        capture: false,
+        append: null,
+        prepend: null,
+        replace: null,
+        captured: null,
         snapshot: state?.snapshot || nanoid(16),
         position: state?.position || scroll.reset(),
-        targets: this.config.targets,
         cache: this.config.cache.enable,
         progress: this.config.progress.threshold,
         threshold: this.config.prefetch.mouseover.threshold,
-        ...state
+
+        // USER OPTIONS
+        //
+        ...state,
+
+        // READ ONLY
+        //
+        targets: this.config.targets
+
       }
 
-      snapshots.set(page.snapshot, snapshot)
-
-      if (dispatchEvent('pjax:cache', page, true)) cache.set(page.url, page)
+      if (page.cache && dispatchEvent('pjax:cache', page, true)) {
+        cache.set(page.url, page)
+        snapshots.set(page.snapshot, snapshot)
+      }
 
       return page
 
-    }
+    },
+
+    /**
+     * Removes cached records. Optionally pass in URL
+     * to remove specific record.
+     *
+     * @param {string} id
+     */
+    snapshot: id => snapshots.get(id),
+
+    /**
+     * Removes cached records. Optionally pass in URL
+     * to remove specific record.
+     *
+     * @param {string} [url]
+     */
+    clear: url => {
+      if (typeof url === 'string') {
+        snapshots.delete(cache.get(url).snapshot)
+        cache.delete(url)
+      } else {
+        snapshots.clear()
+        cache.clear()
+      }
+    },
+
+    /**
+     * Check if cache record exists with snapshot
+     *
+     * @param {string} url
+     * @param {{snapshot?: boolean}} has
+     */
+    get: url => ({
+
+      /**
+       * @returns {Store.IPage}
+       */
+      get page () {
+        return cache.get(url)
+      },
+
+      /**
+       * @returns {string}
+       */
+      get snapshot () {
+        return snapshots.get(this.page.snapshot)
+      }
+
+    }),
+
+    /**
+     * Map setters
+     */
+    get set () {
+
+      return ({
+
+        /**
+         * @param {string} key
+         * @param {Store.IPage} value
+         */
+        cache: (key, value) => cache.set(key, value),
+
+        /**
+         * @param {string} key
+         * @param {string} value
+         */
+        snapshots: (key, value) => snapshots.set(key, value)
+
+      })
+
+    },
+
+    /**
+     * Map Deletions
+     */
+    get delete () {
+
+      return ({
+
+        /**
+         * @param {string} url
+         */
+        cache: url => cache.delete(url),
+
+        /**
+         * @param {string} id
+         */
+        snapshots: id => snapshots.delete(id)
+
+      })
+
+    },
+
+    /**
+     * Check if cache record exists with snapshot
+     *
+     * @param {string} url
+     * @param {{snapshot?: boolean}} has
+     * @return {boolean}
+     */
+    has: (url, has = { snapshot: false }) => (
+
+      !has.snapshot ? cache.has(url) : (
+        cache.has(url) || snapshots.has(cache.get(url)?.snapshot)
+      )
+
+    ),
+
+    /**
+     * Update current pushState History
+     *
+     * @param {string} url
+     * @returns {void}
+     */
+    history: () => (
+
+      history.replace(history.location, {
+        ...history.location.state,
+        position: scroll.position
+      })
+
+    ),
+
+    /**
+     * Updates page state, this function will run a merge
+     * on the current page instance and re-assign the `pageState`
+     * letting to updated config.
+     *
+     * If newState contains a different `ILocation.url` value from
+     * that of the current page instance `url` then it will be updated
+     * to match that of the `newState.url` value.
+     *
+     * The cache will e updated accordingly, so `this.page` will provide
+     * access to the updated instance.
+     *
+     * @param {Store.IPage} state
+     * @returns {Store.IPage}
+     */
+    update: state => (
+
+      cache
+        .set(state.url, merge(cache.get(state.url), state))
+        .get(state.url)
+
+    )
 
   })
 
-})()
+})({
+  targets: [ 'body' ],
+  request: {
+    timeout: 3000,
+    poll: 150,
+    async: true,
+    dispatch: 'mousedown'
+  },
+  prefetch: {
+    mouseover: {
+      enable: true,
+      threshold: 100,
+      proximity: 0
+    },
+    intersect: {
+      enable: true
+    }
+  },
+  cache: {
+    enable: true,
+    limit: 25,
+    save: false
+  },
+  progress: {
+    enable: true,
+    threshold: 500,
+    options: {
+      minimum: 0.10,
+      easing: 'ease',
+      speed: 225,
+      trickle: true,
+      trickleSpeed: 225,
+      showSpinner: false
+    }
+  }
+})
