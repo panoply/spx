07476bbbb40baa7f380deb20831a1ffbb4233e94
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-12-03 18:23:38 +0100

    move to esbuild

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,84 +1,59 @@
-import { defineConfig as Rollup } from 'rollup';
-import { terser } from 'rollup-plugin-terser';
-import noderesolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
-import filesize from 'rollup-plugin-filesize';
-import ts from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
+import { rollup, plugin, env } from '@brixtol/rollup-config';
 
-const { prod } = process.env;
-
-export default Rollup({
-  input: 'src/index.ts',
-  output: [
-    {
-      format: 'es',
-      name: 'Pjax',
-      file: 'package/pjax.esm.js',
-      sourcemap: false,
-      preferConst: true,
-      plugins: prod ? [
-        terser(
+export default rollup(
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        format: 'es',
+        name: 'Pjax',
+        file: 'package/pjax.esm.js',
+        sourcemap: false,
+        preferConst: true
+      },
+      {
+        format: 'umd',
+        name: 'Pjax',
+        file: 'package/pjax.umd.js',
+        sourcemap: false,
+        preferConst: true
+      }
+    ],
+    treeshake: 'smallest',
+    plugins: env.if('dev')(
+      [
+        plugin.esbuild(),
+        plugin.resolve(
           {
-            compress: {
-              passes: 2
+            browser: true,
+            extensions: [
+              '.ts',
+              '.js'
+            ]
+          }
+        ),
+        plugin.replace(
+          {
+            preventAssignment: true,
+            values: {
+              'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
             }
           }
-        )
-      ] : null
-
-    },
-    {
-      format: 'umd',
-      name: 'Pjax',
-      file: 'package/pjax.umd.js',
-      sourcemap: false,
-      preferConst: true,
-      plugins: prod ? [
-        terser(
+        ),
+        plugin.commonjs(
           {
-            compress: {
-              passes: 2
-            }
+            extensions: [
+              '.ts',
+              '.js'
+            ]
           }
         )
-      ] : null
-    }
-  ],
-  plugins: [
-    replace(
-      {
-        preventAssignment: true,
-        values: {
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }
-      }
-    ),
-    noderesolve(
-      {
-        browser: true,
-        extensions: [
-          '.ts',
-          '.js'
-        ]
-      }
-    ),
-    ts(
-      {
-        check: false,
-        useTsconfigDeclarationDir: true,
-        typescript
-      }
-    ),
-    commonjs(
-      {
-        extensions: [
-          '.ts',
-          '.js'
-        ]
-      }
-    ),
-    filesize()
-  ]
-});
+      ]
+    )(
+      [
+        plugin.esminify(),
+        plugin.filesize()
+      ]
+    )
+  }
+);
