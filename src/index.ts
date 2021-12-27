9ed778cb62193da1e1608ad9ec9111c1c26478e0
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-12-27 22:15:38 +0100

    replace arrow function

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ export const supported = !!(
 /**
  * Connect Pjax
  */
-export const connect = (options: IOptions) => {
+export function connect (options: IOptions) {
 
   store.connect(options);
 
