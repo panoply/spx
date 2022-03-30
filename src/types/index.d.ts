d5cf1312be5d6a6b0406a415567ba15ee8bdecef
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-03-30 07:00:29 +0200

    Update index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,5 +2,7 @@ export * from './events';
 export * from './location';
 export * from './methods';
 export * from './options';
+export * from './config';
 export * from './page';
 export * from './progress';
+export * from './selectors';
