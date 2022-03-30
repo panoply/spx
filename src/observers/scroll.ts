151cbab06dbf246bdb75449603a202cde99b6074
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-03-30 07:00:01 +0200

    scroll throttle

--- src/observers/scroll.ts
+++ src/observers/scroll.ts
@@ -1,22 +1,24 @@
 import { IPosition } from '../types/page';
-import { connect } from '../app/connects';
+import { connect } from '../app/state';
+import { create, defineProps } from '../constants/native';
 
 /**
  * Returns to current scroll position, the `reset()`
  * function **MUST** be called after referencing this
  * to reset position.
  */
-let y: number = 0;
-let x: number = 0;
+
 let ticking: boolean = false;
+
+const pos = create(null);
 
 /**
  * Set the current scroll position offset
  * and update the X and Y page references.
  */
-export function position () {
+export function position (): IPosition {
 
-  return { x, y };
+  return pos;
 
 }
 
@@ -24,10 +26,10 @@ export function position () {
  * onScroll event, asserts the current X and Y page
  * offset position of the document
  */
-export function scroll (): void {
+export function onscroll (): void {
 
-  y = window.scrollY;
-  x = window.scrollX;
+  pos.y = window.scrollY;
+  pos.x = window.scrollX;
 
   if (!ticking) {
     window.requestAnimationFrame(position);
@@ -37,17 +39,17 @@ export function scroll (): void {
 }
 
 /**
- * Resets the scroll position`of the document, applying
+ * Resets the scroll position of the document, applying
  * a `x`and `y` positions to `0`
  */
 export function reset (): IPosition {
 
   ticking = false;
 
-  x = 0;
-  y = 0;
+  pos.x = 0;
+  pos.y = 0;
 
-  return { x, y };
+  return pos;
 
 }
 
@@ -58,8 +60,14 @@ export function reset (): IPosition {
  */
 export function y0x0 (): IPosition {
 
-  return { x: 0, y: 0 };
+  const position = create(null);
 
+  defineProps(position, {
+    y: { value: 0, configurable: true, writable: true },
+    x: { value: 0, configurable: true, writable: true }
+  });
+
+  return position;
 }
 
 /**
@@ -67,11 +75,11 @@ export function y0x0 (): IPosition {
  */
 export function start (): void {
 
-  if (!connect.scroll) {
-    scroll();
-    addEventListener('scroll', scroll, { passive: true });
-    connect.scroll = true;
-  }
+  if (connect.has(6)) return;
+
+  onscroll();
+  addEventListener('scroll', onscroll, { passive: true });
+  connect.add(6);
 
 }
 
@@ -80,10 +88,10 @@ export function start (): void {
  */
 export function stop (): void {
 
-  if (connect.scroll) {
-    removeEventListener('scroll', scroll, false);
-    reset();
-    connect.scroll = false;
-  }
+  if (!connect.has(6)) return;
+
+  removeEventListener('scroll', onscroll, false);
+  reset();
+  connect.delete(6);
 
 }
