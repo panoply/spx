9d4636f312bf13e1f3de540626902a891665fe17
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-04-13 19:34:49 +0200

    refactor store logic

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,88 +1,15 @@
 import { nanoid } from 'nanoid';
 import { IPage } from '../types/page';
-import { dispatch } from './events';
-import { emptyObject, join } from './utils';
-import { assign, create as object, define, isArray } from '../constants/native';
-import { pages, snaps, config, selectors } from './state';
+import { emit } from './events';
+import { emptyObject } from './utils';
+import { assign, create as object, isArray } from '../constants/native';
+import { pages, snaps, config } from './state';
+import { ILocation } from 'types';
 import * as scroll from '../observers/scroll';
-import * as progress from './progress';
-import { IConfig, IIntersect, ILocation, IMouseover, IProximity, Options } from 'types';
 
 /**
- * Connects store and intialized the workable
- * state management model. Connect MUST be called
- * upon Pjax initialization. This function acts
- * as a class `constructor` establishing an instance.
- */
-export function initialize (options: Options = {}): IConfig {
-
-  if (options.mouseover !== false) {
-    assign<IMouseover, Options>(config.mouseover, options.mouseover);
-    delete options.mouseover;
-  }
-  if (options.intersect !== false) {
-    assign<IIntersect, Options>(config.intersect, options.intersect);
-    delete options.intersect;
-  }
-  if (options.proximity !== false) {
-    assign<IProximity, Options>(config.proximity, options.proximity);
-    delete options.proximity;
-  }
-  if (options.progress !== false) {
-    assign(config.progress, options.progress);
-    delete options.progress;
-  }
-
-  assign(config, options);
-
-  const attr = config.schema === null ? 'data' : `data-${config.schema}`;
-  const keys = 'hydrate|append|prepend|replace|progress|threshold|position|proximity|ignore|mouseover';
-
-  selectors.attrs = new RegExp(`^${attr}-(${keys})$`, 'i');
-  selectors.eval = `${attr}-eval`;
-  selectors.hydrate = `[${attr}-hydrate]`;
-  selectors.href = `a:not([${attr}-disable]):not([href^="#"])`;
-  selectors.track = `[${attr}-track]:not([${attr}-track=false])`;
-
-  if (typeof config.intersect === 'object') {
-    selectors.intersect = `[${attr}-intersect]:not([${attr}-intersect=false])`;
-    selectors.interhref = `a:not([${attr}-disable]):not(a[${attr}-intersect=false]):not([href^="#"])`;
-  }
-
-  if (typeof config.proximity === 'object') {
-    selectors.proximity = join(
-      'a',
-      `[${attr}-proximity]`,
-      `:not([${attr}-proximity=false])`,
-      `:not([${attr}-disable])`,
-      ':not([href^="#"])'
-    );
-  }
-
-  if (typeof config.mouseover === 'object') {
-    if (config.mouseover.trigger === 'href') {
-      selectors.mouseover = `a:not([${attr}-disable]):not([${attr}-mouseover=false]):not([href^="#"])`;
-    } else {
-      selectors.mouseover = join(
-        'a',
-        `[${attr}-mouseover]`,
-        `:not([${attr}-intersect])`,
-        `:not([${attr}-proximity])`,
-        `:not([${attr}-disable])`,
-        `:not([${attr}-mouseover=false])`,
-        ':not([href^="#"])'
-      );
-    }
-  }
-
-  // Assert Progress
-  if (config.progress !== false) progress.config(config.progress);
-
-  return config;
-
-}
-
-/**
+ * Clear
+ *
  * Removes cached records. Optionally pass in URL
  * to remove specific record.
  */
@@ -114,40 +41,29 @@ export function clear (url?: string[] | string): void {
  * visit and will be overwritten by attribute configs
  * or by any programmatic triggers.
  */
-export function create (state: IPage): IPage {
+export function create (state?: IPage): IPage {
 
   const page: IPage = object(null);
 
-  page.type = 'visit';
-  page.targets = config.targets;
-  page.position = scroll.y0x0();
-  page.title = document.title;
-  page.history = true;
   page.key = null;
-  page.location = null;
+  page.history = true;
+  page.type = 'visit';
+  page.title = document.title;
+  page.replace = config.targets;
+  page.cache = config.cache;
+  page.snapshot = page.cache ? nanoid() : null;
+  page.position = scroll.y0x0();
+  page.location = object(null) as ILocation;
 
   if (config.proximity !== false) page.proximity = config.proximity.threshold;
-  if (config.mouseover !== false) page.threshold = config.mouseover.threshold;
+  if (config.hover !== false) page.threshold = config.hover.threshold;
   if (config.progress !== false) page.progress = config.progress.threshold;
 
-  if (config.cache === false) {
-    page.cache = config.cache;
-    delete state.cache;
-  } else {
-    page.cache = config.cache;
-  }
-
-  // Conditionally generate snapshot
-  if (page.cache && state.cache !== false && !('snapshot' in state)) {
-    page.snapshot = nanoid();
-  } else if (page.cache === false) {
-    page.snapshot = undefined;
-    delete state.snapshot;
-  }
+  if (!page.cache) delete state.cache;
 
   return assign(page, state);
 
-};
+}
 
 /**
  * Check if cache record exists with snapshot
@@ -181,32 +97,29 @@ export function cache (record?: 'page' | 'snapshot' | 'location') {
  */
 export function set (state: IPage, snapshot: string): IPage {
 
-  if (!dispatch('pjax:cache', { state, snapshot }, true)) return;
+  if (!emit('cache', state, snapshot)) return;
 
   pages[state.key] = state;
   cache.prototype.key = state.key;
 
-  if (state.cache) {
-    define(snaps, state.snapshot, {
-      configurable: true,
-      get () { return snapshot; },
-      set (dom) { snapshot = dom; }
-    });
-  }
+  if (state.cache) snaps[state.snapshot] = snapshot;
 
   return state;
 
 }
 
 /**
- * Check if cache record exists with snapshot
+ * Update the page state and snapshots
  */
 export function update (state: IPage, snapshot?: string): IPage {
 
-  const page = assign(pages[state.key], state);
+  const page = state.key in pages
+    ? assign(pages[state.key], state)
+    : create(state);
 
-  if (typeof snapshot === 'string') snaps[page.snapshot] = snapshot;
-
+  if (typeof snapshot === 'string') {
+    snaps[page.snapshot] = snapshot;
+  }
   return page;
 
 }
