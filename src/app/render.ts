634963a594b74b1160ff29aee2de3996c21d782d
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-04-13 19:33:57 +0200

    overhaul script evaluation

--- src/app/render.ts
+++ src/app/render.ts
@@ -1,14 +1,13 @@
 import history from 'history/browser';
-import loadjs from 'loadjs';
-import { forEach } from './utils';
-import { dispatch } from './events';
-import { progress } from './progress';
+import { emit } from './events';
 import { IPage } from '../types/page';
-import { snaps, config, selectors } from './state';
-import { toArray } from '../constants/native';
+import { schema, tracked, config, snaps } from './state';
 import * as store from './store';
-import * as mouseover from '../observers/mouseover';
+import * as mouseover from '../observers/hover';
 import * as intersect from '../observers/intersect';
+import { evaljs } from '../observers/scripts';
+import { toArray } from '../constants/native';
+import * as progress from './progress';
 // import * as proximity from '../observers/proximity';
 
 /**
@@ -17,113 +16,18 @@ import * as intersect from '../observers/intersect';
 const DOMParse: DOMParser = new DOMParser();
 
 /**
- * Tracked Elements
+ * Haad Nodes
+ *
+ * Executes `<head></head>` element replacements
+ * and evaluations.
  */
-const tracked: Set<string> = new Set();
+async function headNodes (target: HTMLHeadElement) {
 
-/**
- * Observe HTML `<head>` tags for mutations, allows us
- * to track scripts, styles and such.
- */
-// @ts-ignore
-// eslint-disable-next-line
-function observeHead(
-  mutations: MutationRecord[],
-  observer: MutationObserver
-): void {
+  const scripts: HTMLScriptElement[] = toArray(target.querySelectorAll(schema.eval));
 
-  mutations.forEach((mutation: MutationRecord) => {
+  scripts.sort((a, b) => a.compareDocumentPosition(b) & Node.DOCUMENT_POSITION_PRECEDING || -1);
 
-    if (mutation.type === 'childList') {
-      mutation.addedNodes.forEach(node => {
-        if (node.nodeName === 'SCRIPT' && node instanceof HTMLElement) {
-          if (node.getAttribute(selectors.eval) !== 'false') {
-            node.setAttribute(selectors.eval, 'false');
-          }
-        }
-      });
-
-    } else if (mutation.type === 'attributes') {
-      console.log(mutation.target);
-
-      console.log(
-        'The ' + mutation.attributeName + ' attribute was modified.'
-      );
-    }
-  });
-}
-
-/**
- * DOM Scripts elements, eg: `<script>` - We will parse
- * all load all scripts for each navigation. We lean on
- * the powerful little module known as `loadjs` for script
- * execution and tracking
- */
-function DOMScripts ({ src, id = src }: HTMLScriptElement): void {
-  if (!loadjs.isDefined(id)) {
-    loadjs(src, id, {
-      before: (_, script) => script.setAttribute(selectors.eval, 'false'),
-      success: () => dispatch('pjax:script', { id }),
-      error: path => console.error(`Pjax: Failed to load script ${path} `),
-      numRetries: 1
-    });
-  }
-}
-
-/**
- * DOM Head Nodes
- */
-function DOMHeadNodes (nodes: string[], { ...children }: HTMLHeadElement): string {
-
-  forEach(DOMNode => {
-    if (DOMNode.tagName === 'TITLE') return null;
-    if (DOMNode.getAttribute(selectors.eval) !== 'false') {
-      const index = nodes.indexOf(DOMNode.outerHTML);
-      index === -1
-        ? DOMNode.parentNode.removeChild(DOMNode)
-        : nodes.splice(index, 1);
-    }
-  }, children);
-
-  return nodes.join('');
-
-}
-
-/**
- * DOM Head
- */
-function DOMHead ({ children }: HTMLHeadElement): void {
-
-  const targetNodes = toArray(children).reduce((arr, node) => {
-
-    if (node.tagName === 'SCRIPT' && node.hasAttribute('src')) {
-      if (node.getAttribute(selectors.eval) !== 'false') {
-        if (node instanceof HTMLScriptElement) {
-          DOMScripts(node);
-          node.parentNode.removeChild(node);
-        }
-      }
-    }
-
-    if (node.tagName !== 'TITLE' && node.tagName !== 'SCRIPT') {
-      arr.push(node.outerHTML);
-    } else {
-      return arr;
-    }
-
-    return arr;
-
-  }, []);
-
-  const fragment = document.createElement('div');
-
-  fragment.innerHTML = DOMHeadNodes(targetNodes, document.head);
-
-  // console.log(fragment.children);
-
-  for (const DOMNode of fragment.children) {
-    if (!DOMNode.hasAttribute(selectors.eval)) document.head.appendChild(DOMNode);
-  }
+  await evaljs(scripts);
 
 }
 
@@ -145,7 +49,7 @@ export function parse (HTMLString: string): Document {
  */
 function trackedNodes (target: HTMLElement): void {
 
-  const nodes = target.querySelectorAll(selectors.track);
+  const nodes = target.querySelectorAll(schema.track);
 
   nodes.forEach((node) => {
 
@@ -155,7 +59,6 @@ function trackedNodes (target: HTMLElement): void {
     if (!tracked.has(node.id)) {
       document.body.appendChild(node);
       tracked.add(node.id);
-      dispatch('pjax:tracked', { target: node }, true);
     }
   });
 
@@ -171,7 +74,7 @@ function trackedNodes (target: HTMLElement): void {
  */
 function renderNodes (state: IPage, target: Document) {
 
-  const nodes = state.replace ? [ ...state.targets, ...state.replace ] : state.targets;
+  const nodes = state.replace ? state.replace : config.targets;
   const selector = nodes.join(',');
   const current = document.body.querySelectorAll<HTMLElement>(selector);
 
@@ -183,14 +86,16 @@ function renderNodes (state: IPage, target: Document) {
   current.forEach((node, i) => {
 
     if (!node.matches(nodes[i])) return;
-    if (!dispatch('pjax:render', { target: node, newTarget: fetched[i] }, true)) return;
+    if (!emit('render', node, fetched[i])) return;
 
     node.replaceWith(fetched[i]);
 
     if (state.append || state.prepend) {
       const fragment = document.createElement('div');
       target.childNodes.forEach(fragment.appendChild);
-      return state.append ? node.appendChild(fragment) : node.insertBefore(fragment, node.firstChild);
+      return state.append
+        ? node.appendChild(fragment)
+        : node.insertBefore(fragment, node.firstChild);
     }
 
   });
@@ -214,10 +119,12 @@ function hydrateNodes (state: IPage, target: Document): void {
   const current = document.body.querySelectorAll<HTMLElement>(nodes);
 
   if (current.length > 0) {
+
     const fetched = target.body.querySelectorAll<HTMLElement>(nodes);
+
     current.forEach((node, i) => {
 
-      if (!dispatch('pjax:hydrate', { target: node, newTarget: fetched[i] }, true)) return;
+      if (!emit('hydrate', node, fetched[i])) return;
 
       // InnerHTML replacment on text nodes
       if (node.firstChild.nodeType === Node.TEXT_NODE) {
@@ -225,6 +132,7 @@ function hydrateNodes (state: IPage, target: Document): void {
       } else {
         node.replaceWith(fetched[i]);
       }
+
     });
   }
 
@@ -237,33 +145,21 @@ function hydrateNodes (state: IPage, target: Document): void {
 }
 
 /**
- * Observe Head Element
- */
-// const observer = new MutationObserver(observeHead);
-
-/**
- * Observe Head Element
- */
-// observer.observe(document.head, {
-//  attributes: true,
-//  childList: true,
-//  subtree: true
-// });
-
-/**
  * Update the DOM and execute page adjustments
  * to new navigation point
  */
 export function update (state: IPage, popstate?: boolean): IPage {
 
-  if (config.mouseover !== false) mouseover.stop();
+  // head.stop();
+
+  if (config.hover !== false) mouseover.stop();
   if (config.intersect !== false) intersect.stop();
   // if (config.proximity !== false) proximity.stop();
 
   const target = parse(snaps[state.snapshot]);
   state.title = document.title = target.title || '';
 
-  if (target.head) DOMHead(target.head);
+  headNodes(target.head);
 
   if (state.hydrate) {
 
@@ -290,11 +186,11 @@ export function update (state: IPage, popstate?: boolean): IPage {
 
   progress.done();
 
-  if (config.mouseover !== false) mouseover.start();
+  if (config.hover !== false) mouseover.start();
   if (config.intersect !== false) intersect.start();
   // if (config.proximity !== false) proximity.start();
 
-  dispatch('pjax:load', state);
+  emit('load', state);
 
   return state;
 
