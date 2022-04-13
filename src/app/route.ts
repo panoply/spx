65a1066400c13fd988bf82afb12a17d5e46e766a
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-04-13 19:34:23 +0200

    refactor route logic

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,4 +1,4 @@
-import { selectors } from './state';
+import { schema } from './state';
 import * as regex from '../constants/regexp';
 import { IPage } from '../types/page';
 import { nil, create } from '../constants/native';
@@ -7,7 +7,17 @@ import history from 'history/browser';
 import { parsePath, createPath } from 'history';
 import { ILocation } from '../types/location';
 
-export const { origin, hostname } = window.location;
+/**
+ * Origin
+ *
+ * Returns the location domain origin, eg: https://brixtol.com
+ */
+export const { origin } = window.location;
+
+/**
+ * Location hostname eg: https://brixtol.com
+ */
+export const hostname = origin.replace(regex.Protocol, nil);
 
 /**
  * Get Attributes
@@ -21,7 +31,7 @@ export function getAttributes ({ attributes }: Element): IPage {
 
   for (const { nodeName, nodeValue } of attributes) {
 
-    if (!selectors.attrs.test(nodeName)) continue;
+    if (!schema.attrs.test(nodeName)) continue;
 
     const name = nodeName.slice(1 + nodeName.lastIndexOf('-'));
     const value = nodeValue.replace(/\s+/g, nil);
@@ -39,6 +49,7 @@ export function getAttributes ({ attributes }: Element): IPage {
     } else {
       state[name] = value;
     }
+
   }
 
   return state;
@@ -52,28 +63,41 @@ export function getAttributes ({ attributes }: Element): IPage {
  * reference which is used as property id
  * in the cache store.
  */
-export function getKey (link?: string | Element): string {
+export function getKey (link?: string): string {
 
-  const key = typeof link === 'undefined'
-    ? createPath(history.location)
-    : typeof link === 'string'
-      ? link
-      : link.getAttribute('href');
+  // 47 is unicode value for '/', eg: '/path'
+  if (link.charCodeAt(0) === 47 && link.charCodeAt(1) !== 47) {
 
-  // 47 is unicode value for '/'
-  if (key.charCodeAt(0) === 47) return key;
+    // Strips any hash references from url
+    const hash = link.indexOf('#');
+    return hash !== -1 ? link.slice(0, hash) : link;
+  }
 
-  // 63 is unicode value for '?'
-  if (key.charCodeAt(0) === 63) return history.location.pathname + key;
+  // 63 is unicode value for '?', eg: '?param'
+  if (link.charCodeAt(0) === 63) return history.location.pathname + link;
 
-  const path = key.match(regex.Pathname);
+  // Handle url references, eg: 'https://' or '//'
+  const url = link.match(regex.Pathname);
 
-  return path === null ? '/' : path[1];
+  if (url !== null) {
+
+    // Invalid hostname, eg: github.com !== brixtol.com
+    if (hostname !== url[1]) return null;
+
+    // Valid hostname, pointing to index, eg: https://brixtol.com
+    if (url[2] === undefined) return '/';
+
+    return url[2];
+
+  }
+
+  // Unable to process, pass null
+  return null;
 
 };
 
 /**
- * Get Location
+ * Get Locationæ™
  *
  * Parses link and returns an ILocation.
  */
@@ -98,22 +122,31 @@ export function getLocation (path: string): ILocation {
  *
  * Parses link and returns page state. When `link`
  * is a HTML Element (ie: `<a href=""></a>`) then
- * we parse the node assign any attributes. This function
- * is triggered for every visit request.
+ * we parse the node and assign any attributes.
+ *
+ * This function is triggered for every visit request
+ * or action which infers navigations, ie: mouseover.
  */
 export function getRoute (link?: Element | string, type?: string): IPage {
-
-  const path = typeof link === 'string' ? link : typeof link === 'undefined'
-    ? createPath(history.location)
-    : link.getAttribute('href');
 
   const state: IPage = link instanceof Element
     ? getAttributes(link)
     : create(null);
 
+  const path = typeof link === 'string'
+    ? link
+    : typeof link === 'undefined'
+      ? createPath(history.location)
+      : link.getAttribute('href');
+
   state.key = getKey(path);
   state.location = getLocation(state.key);
-  state.type = typeof type === 'undefined' ? path === state.location.lastpath ? 'initial' : type : 'visit';
+
+  state.type = typeof type === 'undefined'
+    ? path === state.location.lastpath
+      ? 'initial'
+      : type
+    : 'visit';
 
   return state;
 
