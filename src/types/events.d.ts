10269a3789adb324e149b37bc2eccd8cce4f8b18
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-04-14 07:04:16 +0200

    expose events types

--- src/types/events.d.ts
+++ src/types/events.d.ts
@@ -1,4 +1,5 @@
-import { ILocation } from './location';
+/* eslint-disable no-unused-vars */
+
 import { IPage } from './page';
 
 /**
@@ -16,142 +17,160 @@ export type EventNames = (
  | 'load'
 );
 
-declare type ConnectedEvent = (state: IPage) => void
-declare type TriggerEvent = (event: MouseEvent, route: IPage) => void | false
-declare type PrefetchEvent = (state: IPage, type: 'hover' | 'intersect' | 'proximity') => void | false
-declare type RequestEvent = (state: IPage, type: 'hover' | 'intersect' | 'proximity') => void | false
-declare type CacheEvent = (state: IPage, snapshot: Document) => void | false | Document
-declare type HydrateEvent = (element: Element, newElement: Element) => void | false
-declare type RenderEvent = (element: Element, newElement: Element) => void | false
-declare type LoadEvent = (state: IPage) => void
-
-export type LifecycleEvent<T extends EventNames> = (
-  T extends 'connected' ? ConnectedEvent :
-  T extends 'trigger' ? TriggerEvent :
-  T extends 'prefetch' ? PrefetchEvent :
-  T extends 'request' ? RequestEvent :
-  T extends 'cache' ? CacheEvent :
-  T extends 'hydrate' ? HydrateEvent:
-  T extends 'render' ? RenderEvent:
-  T extends 'load' ? LoadEvent : never
+/**
+ * Event Type
+ *
+ * Enum number value is passed. Below is the id reference
+ *
+ * 1. Click/Mousedown event
+ * 2. Prefetch hover event
+ * 3. Prefetch intersection
+ * 4. Prefetch proximity
+ * 5. Prefetch preload
+ * 6. Reverse cache fetch (lastpath)
+ * 7. Popstate fetch
+ * 8. Programmatic reload (method trigger)
+ * 9. Programmatic prefetch (method trigger)
+ */
+export type EventType = (
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
 )
 
 /**
- * Prefetch Event
+ * Emitter Arguments
  */
-export interface IPrefetch {
-  /**
-   * The `<a>` target element being fetched
-   */
-  target: HTMLLinkElement;
-  /**
-   * The key URL (pathname) identifier
-   */
-  url: string;
-  /**
-   * Location URL object
-   */
-  location: ILocation
-}
+export type EmitterArguments<T extends EventNames> = (
+  T extends 'connected' ? [
+    state: IPage
+  ] :
+  T extends 'trigger' ? [
+    event: MouseEvent,
+    state: IPage
+  ] :
+  T extends 'prefetch' ? [
+    target: Element,
+    state: IPage,
+    type: EventType
+  ] :
+  T extends 'request' ? [
+    state: IPage,
+    type: EventType
+  ] :
+  T extends 'cache' ? [
+    state: IPage,
+    snapshot: string
+  ] :
+  T extends 'hydrate' ? [
+    element: Element,
+    newElement: Element
+  ] :
+  T extends 'render' ? [
+    element: Element,
+    newElement: Element
+  ] :
+  T extends 'load' ? [
+    state: IPage
+  ] : never
+)
 
 /**
- * Trigger Event
+ * Lifecycle Events
  */
-export interface ITrigger {
-  /**
-   * The clicked `<a>` target element
-   */
-  target: HTMLLinkElement;
-}
+export type LifecycleEvent<T extends EventNames> = (
 
-/**
- * Request Event
- */
-export interface IRequest {
-  /**
-   * The page state object
-   */
-  readonly state: IPage;
-}
+  T extends 'connected' ? (
+    /**
+     * Page state reference
+     */
+    state?: IPage
+  ) => void :
 
-/**
- * Render Event
- */
-export interface IRender {
-  /**
-   * The current target document
-   */
-  target: HTMLElement;
-  /**
-   * The fragment which will replace `target`
-   */
-  newTarget: HTMLElement;
-}
+  T extends 'trigger' ? (
+    /**
+     * The mouse event, access target via `event.target`
+     */
+    event?: MouseEvent,
+    /**
+     * Page state reference
+     */
+    route?: IPage
 
-/**
- * Hydrate Event
- */
-export interface IHydrate {
-  /**
-   * The current target document
-   */
-  target: HTMLElement;
-  /**
-   * The new target document
-   */
-  newTarget: HTMLElement;
-}
+  ) => void | false :
 
-/**
- * Tracked Event
- */
-export interface ITracked {
-  /**
-   * The node element marked tracked
-   */
-  target: HTMLElement
-}
+  T extends 'prefetch' ? (
+    /**
+     * Page state reference
+     */
+    state?: IPage,
+    /**
+     * Event dispatch type id
+     */
+    type?: Exclude<EventType, 'trigger'>
 
-/**
- * Load Event
- */
-export interface ILoad {
-  /**
-   * The page state object
-   */
-  readonly state: IPage;
-}
+  ) => void | false :
 
-/**
- * Custom Event Wrapper
- */
-export namespace Events {
-  /**
-   * `pjax:prefetch`
-   */
-  export type Prefetch = CustomEvent<IPrefetch>;
-  /**
-   * `pjax:trigger`
-   */
-  export type Trigger = CustomEvent<ITrigger>;
-  /**
-   * `pjax:request`
-   */
-  export type Request = CustomEvent<IRequest>;
-  /**
-   * `pjax:render`
-   */
-  export type Render = CustomEvent<IRender>;
-  /**
-   * `pjax:tracked`
-   */
-  export type Tracked = CustomEvent<ITracked>;
-  /**
-   * `pjax:hydrate`
-   */
-  export type Hydrate = CustomEvent<IHydrate>;
-  /**
-   * `pjax:load`
-   */
-  export type Load = CustomEvent<ILoad>;
-}
+  T extends 'request' ? (
+    /**
+     * Page state reference
+     */
+     state?: IPage,
+     /**
+      * Event dispatch type id
+      */
+     type?: EventType
+
+  ) => void | false :
+
+  T extends 'cache' ? (
+    /**
+     * Page state reference
+     */
+    state?: IPage,
+    /**
+     * Parsed document snapshot. Augment the snapshot
+     * by returning the document.
+     */
+    snapshot?: Document
+
+  ) => void | false | Document :
+
+  T extends 'hydrate' ? (
+    /**
+     * The target element that will be replaced
+     */
+    element?: Element,
+    /**
+     * The element replacing the current target
+     */
+    newElement?: Element
+
+  ) => void | false :
+
+  T extends 'render' ? (
+    /**
+     * The target element that will be replaced
+     */
+     element?: Element,
+     /**
+      * The element replacing the current target
+      */
+     newElement?: Element
+
+  ) => void | false :
+
+  T extends 'load' ? (
+    /**
+     * Page state reference
+     */
+    state?: IPage
+
+  ) => void : never
+)
