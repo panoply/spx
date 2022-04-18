9b3bc214b1c7d45cfec0bb059ab75b255dfbb86c
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-04-18 04:25:24 +0200

    use native history API

---
+++ src/shared/native.ts
@@ -0,0 +1,41 @@
+import { History } from 'types';
+
+/**
+ * History API `window.history`
+ */
+export const history = window.history as History;
+
+/**
+ * The location origin, eg: `https://brixtol.com`
+ */
+export const origin = window.location.origin;
+
+/**
+ * Cached `Object.getOwnPropertyNames`
+ */
+export const props = Object.getOwnPropertyNames;
+
+/**
+ * Cached `Object.assign`
+ */
+export const assign = Object.assign;
+
+/**
+  * Cached `Object.create`
+  */
+export const object = Object.create;
+
+/**
+ * Cached `Array.isArray`
+ */
+export const isArray = Array.isArray;
+
+/**
+ * Cached `Array.from`
+ */
+export const toArray = Array.from;
+
+/**
+ * Empty string value
+ */
+export const nil = '';
