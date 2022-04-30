cbea89efcb15542e4a4cf6deb18ad1f7f3e4f4ff
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-04-30 23:26:07 +0200

    Deploy updates

--- bin/docs.sh
+++ bin/docs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+root=../
 dir=docs
 public=docs/public
 branch=docs
 
 generate() {
-  jekyll build
+  pnpm build
 }
 
 echo -e "\033[0;32mDeleting docs/public...\033[0m"
@@ -13,11 +14,14 @@ rm -rf $public
 echo -e "\033[0;32mChecking out $branch....\033[0m"
 git worktree add $public $branch
 
+echo -e "\033[0;32mChanging Directory...\033[0m"
+cd $dir
+
 echo -e "\033[0;32mGenerating Documentation...\033[0m"
-cd $dir && build_command &&
+generate
 
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-cd $directory && git add --all && git commit -m "Deploy updates" && git push origin $branch
+git add --all && git commit -m "Deploy updates" && git push origin $branch
 
 echo -e "\033[0;32mCleaning up...\033[0m"
 git worktree remove $directory
