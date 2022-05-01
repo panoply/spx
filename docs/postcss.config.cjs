fcf579e995351f07293d30ff2a685b15778d5046
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-05-01 05:59:30 +0200

    cssnano

--- docs/postcss.config.cjs
+++ docs/postcss.config.cjs
@@ -1,9 +1,13 @@
 const purge = require('@fullhuman/postcss-purgecss')
 const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
 module.exports = {
 	plugins: [
     autoprefixer(),
+    cssnano({
+      preset: 'default',
+    }),
     purge(
       {
         variables: true,
