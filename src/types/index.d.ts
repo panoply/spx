22fa72e9fcb1abb908e0480e1e04b61d85f2aa06
Nikolas Savvidis <nicos@gmx.com>
2023-06-26 09:32:19 +0200

    expose globalThis type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,24 @@
+/* eslint-disable no-unused-vars */
+import SPX from './methods';
 export * from './events';
 export * from './methods';
 export * from './options';
 export * from './config';
 export * from './page';
 export * as default from './methods';
+
+declare global {
+
+  interface Window {
+    /**
+     * **_SPX_**
+     */
+    spx: typeof SPX
+  }
+
+  /**
+   * **_SPX_**
+   */
+ export const spx: typeof SPX;
+
+}
