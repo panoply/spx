a41bc564bc3a86247d79036c5cafc910fde4aa33
Nikolas Savvidis <nicos@gmx.com>
2024-03-11 11:22:55 +0100

    decouple component lifecycle method hooks
    ensure async resolvers are handled gracefully

--- src/observe/components.ts
+++ src/observe/components.ts
@@ -1,12 +1,16 @@
-import { getComponents, setInstances } from '../components/context';
+/* eslint-disable no-unused-vars */
+import { getComponents } from '../components/context';
+import { setInstances } from '../components/instances';
 import { removeEvent } from '../components/listeners';
 import { context, resetContext } from '../components/observe';
-import { Errors, VisitType } from '../shared/enums';
+import { LogType, VisitType } from '../shared/enums';
 import { log } from '../shared/logs';
 import { $ } from '../app/session';
 import { toArray } from '../shared/native';
-import { patch } from '../app/queries';
+import { get, patchPage } from '../app/queries';
 import { onNextTick } from '../shared/utils';
+import { Page } from '../types/page';
+import { SPX } from '../types/components';
 
 export type Lifecycle = (
   | 'oninit'
@@ -18,51 +22,90 @@ export type Lifecycle = (
   | 'oncache'
 )
 
+const enum HookMethod {
+  SYNC = 1,
+  ONLOAD = 2,
+  ONEXIT = 3
+}
+
 export function teardown () {
 
-  for (const ref in $.components.reference) {
-    delete $.components.reference[ref];
+  for (const ref in $.components.$reference) {
+    delete $.components.$reference[ref];
   }
 
-  for (const instance of $.components.instances.values()) {
+  for (const instance of $.components.$instances.values()) {
     for (const key in instance.scope.events) {
       removeEvent(instance, instance.scope.events[key]);
     }
   }
 
-  $.components.elements.clear();
-  $.components.instances.clear();
-  $.components.connected.clear();
+  $.components.$elements.clear();
+  $.components.$instances.clear();
+  $.components.$connected.clear();
 
-  log(Errors.INFO, 'Component instances were disconnected');
+  log(LogType.INFO, 'Component instances were disconnected');
 
 }
 
-export function hook (event: Lifecycle, params: any) {
+export function mount (promises: [instance: SPX.Class, method: string][]) {
 
-  if (event === 'onvisit') {
-    return onNextTick(() => hook(null, params));
-  } else if (event === null) {
-    event = 'onvisit';
+  const promise = promises.map(([ instance, method ]) => (
+    instance.scope.mounted
+      ? instance[method]($.page)
+      : Promise.reject<string>(instance.scope.instanceOf)
+  ));
+
+  return Promise.race(promise);
+
+}
+
+export function hook (event: Lifecycle, params: any, cb?: () => void) {
+
+  if (event === 'onvisit') return onNextTick(() => hook(null, params));
+
+  const { $connected, $instances } = $.components;
+
+  let method: HookMethod = HookMethod.SYNC;
+  let promises: [instance: SPX.Class, method: string][] = [];
+
+  if (event === null) event = 'onvisit';
+  else if (event === 'onload') {
+    method = HookMethod.ONLOAD;
+    promises = [];
+    patchPage('components', toArray($connected));
+  } else if (event === 'onexit') {
+    method = HookMethod.ONEXIT;
+    promises = [];
   }
 
-  const { connected, instances } = $.components;
-
-  if (event === 'onload') {
-    patch('components', toArray(connected));
-  }
-
-  for (const uuid of connected) {
-    const instance = instances.get(uuid);
+  for (const uuid of $connected) {
+    const instance = $instances.get(uuid);
     if (instance && event in instance) {
-      instance[event].apply(instance, params);
+      if (method === HookMethod.SYNC) {
+        instance[event](params);
+      } else {
+        promises.push([ instance, event ]);
+      }
     }
   }
 
+  if (promises.length > 0) {
+    mount(promises).then(() => {
+      for (const [ instance ] of promises) {
+        instance.scope.mounted = method === HookMethod.ONLOAD;
+      }
+    }).catch(() => {
+      for (const [ instance ] of promises) {
+        instance.scope.mounted = false;
+      }
+    });
+  }
 }
 
 export function connect () {
 
+  if (!$.config.components) return;
   if ($.observe.components) return;
 
   if ($.page.type === VisitType.INITIAL) {
@@ -72,12 +115,17 @@ export function connect () {
   } else {
 
     if (context) {
-      setInstances(context);
-      resetContext();
+
+      setInstances(context).then(() => {
+        hook('onload', $.page);
+        resetContext();
+      });
+
+    } else {
+
+      hook('onload', $.page);
+
     }
-
-    hook('onload', [ $.page ]);
-
   }
 
   $.observe.components = true;
@@ -88,7 +136,7 @@ export function disconnect () {
 
   if (!$.observe.components) return;
 
-  hook('onexit', [ $.page ]);
+  hook('onexit', $.page);
 
   $.observe.components = false;
 
