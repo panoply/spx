6252b2aaff3a848e83fd898ff9db9873853689a3
Nikolas Savvidis <nicos@gmx.com>
2024-03-11 11:22:12 +0100

    concluded algo but still more to be done

--- src/morph/snapshot.ts
+++ src/morph/snapshot.ts
@@ -1,135 +1,169 @@
-import type { IComponentBinds } from '../types/components';
+import type { ComponentBinds } from '../types/components';
 import { isDirective } from '../components/context';
-import { escSelector, onNextTick } from '../shared/utils';
 import { log } from '../shared/logs';
-import { Errors } from '../shared/enums';
+import { LogType } from '../shared/enums';
 import { $ } from '../app/session';
 import * as q from '../app/queries';
+import { walkElements } from './walk';
 
-export function markSnap (nodes: string[]) {
+// export function markComponents (datasets: Array<[string, string]>) {
 
-  onNextTick(() => morphSnap(q.getSnapDom($.page.key).body, nodes));
+//   const { instances } = $.components;
+//   const { page, dom } = q.get();
+//   const snapDom = dom.body;
 
-}
+//   for (const [ selector, key ] of datasets) {
 
-export function markComponents (datasets: Array<[string, string]>) {
+//     const instance = instances.get(key);
+//     const attrRef = instance.dom.getAttribute($.qs.$ref);
+//     const element = snapDom.querySelector(selector);
+//     const {
+//       instanceOf,
+//       binds,
+//       nodes
+//     } = instance.scope;
 
-  const { instances } = $.components;
-  const { page, dom } = q.get();
-  const snapDom = dom.body;
+//     for (const ref in binds) {
 
-  for (const [ selector, key ] of datasets) {
+//       const bind = binds[ref];
+//       const selector = `[${$.qs.$bind}=${escSelector(`${instanceOf}.${bind.stateKey}`)}]`;
+//       const node = (bind.isChild ? element : snapDom).querySelector(selector);
 
-    const instance = instances.get(key);
-    const attrRef = instance.dom.getAttribute($.qs.$ref);
-    const element = snapDom.querySelector(selector);
-    const {
-      instanceOf,
-      binds,
-      nodes
-    } = instance.scope;
+//       if (node.getAttribute($.qs.$ref) !== bind.key) {
+//         node.setAttribute($.qs.$ref, bind.key);
+//         log(LogType.VERBOSE, `Binding reference in snapshot ${bind.key} was applied`, '#fff');
+//       }
+//     }
 
-    for (const ref in binds) {
+//     for (const ref in nodes) {
 
-      const bind = binds[ref];
-      const selector = `[${$.qs.$bind}=${escSelector(`${instanceOf}.${bind.stateKey}`)}]`;
-      const node = (bind.isChild ? element : snapDom).querySelector(selector);
+//       const item = nodes[ref];
+//       const selector = `[${$.qs.$node}*=${escSelector(`${instanceOf}.${item.keyProp}`)}]`;
+//       const node = (item.isChild ? element : snapDom).querySelector(selector);
 
-      if (node.getAttribute($.qs.$ref) !== bind.key) {
-        node.setAttribute($.qs.$ref, bind.key);
-        log(Errors.TRACE, `Binding reference in snapshot ${bind.key} was applied`, '#fff');
-      }
-    }
+//       if (node.getAttribute($.qs.$ref) !== item.key) {
+//         node.setAttribute($.qs.$ref, item.key);
+//         log(LogType.VERBOSE, `Node reference in snapshot ${item.key} was applied`, '#fff');
+//       }
+//     }
 
-    for (const ref in nodes) {
+//     if (element.getAttribute($.qs.$ref) !== attrRef) {
+//       element.setAttribute($.qs.$ref, attrRef);
+//       log(LogType.VERBOSE, `Component reference in snapshot ${page.snap} was applied`, '#fff');
+//     }
+//   }
 
-      const item = nodes[ref];
-      const selector = `[${$.qs.$node}*=${escSelector(`${instanceOf}.${item.keyProp}`)}]`;
-      const node = (item.isChild ? element : snapDom).querySelector(selector);
+//   q.setSnap(dom.documentElement.outerHTML);
 
-      if (node.getAttribute($.qs.$ref) !== item.key) {
-        node.setAttribute($.qs.$ref, item.key);
-        log(Errors.TRACE, `Node reference in snapshot ${item.key} was applied`, '#fff');
-      }
-    }
-
-    if (element.getAttribute($.qs.$ref) !== attrRef) {
-      element.setAttribute($.qs.$ref, attrRef);
-      log(Errors.TRACE, `Component reference in snapshot ${page.snap} was applied`, '#fff');
-    }
-  }
-
-  q.setSnap(dom.documentElement.outerHTML);
-
-}
+// }
 
 /**
  * Morph Bindings
  *
  * Updates `spx-bind` elements in snapshot references.
  */
-export function morphBinds (cRef: string, bind: IComponentBinds, value: string) {
+export function morphBinds (cRef: string, bind: ComponentBinds, value: string) {
 
   const { page, dom } = q.get();
   const { body } = dom;
 
   body.querySelector<HTMLElement>(bind.selector).innerText = value;
 
-  log(Errors.TRACE, `Binding node in snapshot was updated: ${value}`);
+  log(LogType.VERBOSE, `Binding node in snapshot was updated: ${value}`);
 
   $.snaps[page.snap] = dom.documentElement.outerHTML;
 
 }
+
+export function patchSnap (snapNode: Element, pageNode: Element, nodes: string[], morphs: Set<Element>) {
+
+  let i: number;
+  let s: Element;
+  let d: Element;
+
+  if (snapNode.firstElementChild) {
+    i = 0;
+    s = snapNode.children[i];
+    d = pageNode.children[i];
+  }
+
+  while (s) {
+
+    if (s) {
+
+      if (morphs.has(d)) {
+
+        morphSnap(s, nodes, true);
+        morphs.delete(d);
+
+      } else if (d.hasAttribute($.qs.$ref)) {
+
+        s.setAttribute(
+          $.qs.$ref,
+          d.getAttribute($.qs.$ref)
+        );
+
+      }
+
+      patchSnap(s, d, nodes, morphs);
+
+    }
+
+    s = snapNode.children[++i];
+    d = pageNode.children[i];
+
+  }
+
+};
 
 /**
  * Morph Snapshot
  *
  * Walks the snapshot and update DOM Elements with reference datasets.
  */
-export function morphSnap (snapNode: Element, nodes: string[]) {
+export function morphSnap (snapNode: Element, nodes: string[], isPatch = false) {
 
-  let i: number;
-  let s: Element;
+  const { $elements } = $.components;
 
-  if (snapNode.firstElementChild) {
-    i = 0;
-    s = snapNode.children[i];
-  }
+  walkElements(snapNode, (node) => {
 
-  while (s) {
+    if (node.getAttribute('spx-snapshot') === 'false') return;
 
-    if ((s.nodeName === 'svg' || s.nodeName === 'CODE') && s.childElementCount > 0) {
-      i = 0;
-      s = s.nextElementSibling;
-    }
+    if (isDirective(node.attributes) && !node.hasAttribute($.qs.$ref)) {
 
-    if (s) {
+      const nodeRef = nodes.shift();
 
-      if (isDirective(s.attributes)) {
+      if (!nodeRef) {
+        log(LogType.ERROR, 'Undefined reference, the snapshot record failed to align', node);
+      }
 
-        const nodeRef = nodes.shift();
-        const domNode = $.components.elements.get(nodeRef);
-        const snapAttr = s.getAttribute($.qs.$ref);
+      if ($elements.has(nodeRef)) {
+
+        const snapAttr = node.getAttribute($.qs.$ref);
+        const domNode = $elements.get(nodeRef);
         const domAttr = domNode.getAttribute($.qs.$ref);
 
         if (snapAttr !== domAttr) {
-          s.setAttribute($.qs.$ref, domNode.getAttribute($.qs.$ref));
+          node.setAttribute($.qs.$ref, domAttr);
         }
 
         if (nodes.length === 0) {
-          q.setSnap(s.ownerDocument.documentElement.outerHTML);
-          log(Errors.TRACE, 'Snapshot updated');
-          break;
+
+          if (isPatch === false) {
+            q.setSnap(node.ownerDocument.documentElement.outerHTML);
+            log(LogType.VERBOSE, `Snapshot ${$.page.key} updated for: ${$.page.snap}`);
+          }
+
+          return false;
+
         }
+
+      } else {
+        log(LogType.ERROR, 'Undefined reference, the snapshot record failed to align', node);
       }
-
-      morphSnap(s, nodes);
-
     }
 
-    s = snapNode.children[++i];
-
-  }
+  });
 
 };
 
@@ -149,9 +183,9 @@ export function morphHead (method: 'removeChild' | 'appendChild', newNode: HTMLE
 
     $.snaps[page.snap] = dom.documentElement.outerHTML;
 
-    log(Errors.TRACE, `Snapshot record was updated. Node ${operation} from <head>`, newNode);
+    log(LogType.VERBOSE, `Snapshot record was updated. Node ${operation} from <head>`, newNode);
   } else {
-    log(Errors.WARN, 'Node does not exists in snapshot record, no mutation applied', newNode);
+    log(LogType.WARN, 'Node does not exists in snapshot record, no mutation applied', newNode);
   }
 
 }
