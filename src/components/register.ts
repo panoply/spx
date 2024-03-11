7cef014090899d5f2220690f9990531c8df63e06
Nikolas Savvidis <nicos@gmx.com>
2024-03-11 11:21:25 +0100

    Update register.ts

--- src/components/register.ts
+++ src/components/register.ts
@@ -1,46 +1,49 @@
 import { camelCase, downcase, hasProp, hasProps } from '../shared/utils';
 import { $ } from '../app/session';
-import { Errors } from '../shared/enums';
-import { IComponentRegister } from '../types/components';
+import { Colors, LogType } from '../shared/enums';
+import { ComponentRegister } from '../types/components';
 import { log } from '../shared/logs';
+import { Merge } from 'type-fest';
 
-export function getComponentIdentifier (instance: IComponentRegister & { name?: string }, identifier?: string) {
+type Register = Merge<ComponentRegister, {
+  /**
+   * Component class name
+   */
+  name?: string
+}>
 
-  const { name } = instance;
+/**
+ * Normalize Component
+ *
+ * Ensures that component identifiers and internal logic has been correctly
+ * composed into a format the SPX can understand. Specifically, this function
+ * will align identifiers and assign missing `static connect` references.
+ *
+ * The `identifer` parameter is optional here, when `undefined` we will obtain
+ * id reference from either the instance `static connect` or the component class name.
+ */
+export function getComponentId (instance: Register, identifier?: string) {
+
+  const name = instance.name;
   const original = identifier;
 
-  if (!identifier) {
-    identifier = downcase(instance.name);
-  }
+  identifier = downcase(identifier || name);
 
   if (!hasProp(instance, 'connect')) {
-    instance.connect = {
-      id: identifier,
-      state: {},
-      nodes: []
-    };
+    instance.connect = { id: identifier, state: {}, nodes: [] };
   }
 
   const has = hasProps(instance.connect);
 
-  if (!has('state')) {
-    instance.connect.state = {};
-  }
-
-  if (!has('nodes')) {
-    instance.connect.nodes = [];
-  }
-
-  if (!has('id')) {
-    instance.connect.id = identifier;
-  }
-
+  if (!has('state')) instance.connect.state = {};
+  if (!has('nodes')) instance.connect.nodes = [];
+  if (!has('id')) instance.connect.id = identifier;
   if (identifier !== instance.connect.id) {
     identifier = camelCase(instance.connect.id);
   }
 
   if (name !== original && /^[A-Z]|[_-]/.test(instance.connect.id)) {
-    log(Errors.WARN, [
+    log(LogType.WARN, [
       `Component identifer name "${instance.connect.id}" must use camelCase format.`,
       `The identifer has been converted to "${identifier}"`
     ]);
@@ -53,22 +56,25 @@ export function getComponentIdentifier (instance: IComponentRegister & { name?: 
 /**
  * Register Components
  *
- * Component registar, exposed on the global level, assigns each component caller.
+ * Component registration. This function assigns each component caller within the
+ * `$.components.register` Map store. The `isValidID` parameter will determine whether
+ * or not we should call `getComponentId()` to obtain identifier. When `isValidID` is `true`
+ * it signals that component was registered via `spx.register()` method and analysis has
+ * already taken place,
  */
-export function registerComponents (components: { [component: string]: IComponentRegister }) {
+export function registerComponents (components: { [id: string]: Register }, isValidID = false) {
 
-  const { registry } = $.components;
+  const { $registry } = $.components;
 
   for (const id in components) {
-
     const instance = components[id];
-    const identifier = getComponentIdentifier(instance, id);
-
-    if (!registry.has(identifier)) {
-      registry.set(identifier, instance);
-      log(Errors.TRACE, `${id} component registered under: ${identifier}`);
+    const identifier = isValidID ? id : getComponentId(instance, id);
+    if (!$registry.has(identifier)) {
+      $registry.set(identifier, instance);
+      log(LogType.VERBOSE, `Component ${instance.name} registered using id: ${identifier}`, Colors.PINK);
     }
-
   }
+
+  if (!$.config.components) $.config.components = true;
 
 }
