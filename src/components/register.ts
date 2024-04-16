f7b0c7fefa49a40c4b0baf082e5eb4bba8b552ad
Nikolas Savvidis <nicos@gmx.com>
2024-04-16 06:40:58 +0200

    align component register with "name" static ref

--- src/components/register.ts
+++ src/components/register.ts
@@ -16,7 +16,7 @@ type Register = Merge<ComponentRegister, {
  *
  * Ensures that component identifiers and internal logic has been correctly
  * composed into a format the SPX can understand. Specifically, this function
- * will align identifiers and assign missing `static connect` references.
+ * will align identifiers and assign missing `static define` references.
  *
  * The `identifer` parameter is optional here, when `undefined` we will obtain
  * id reference from either the instance `static connect` or the component class name.
@@ -29,19 +29,23 @@ export function getComponentId (instance: Register, identifier?: string) {
   identifier = downcase(identifier || name);
 
   if (!hasProp(instance, 'define')) {
-    instance.define = { id: identifier, state: {}, nodes: [] };
+    instance.define = {
+      name: identifier,
+      state: {},
+      nodes: []
+    };
   }
 
   const has = hasProps(instance.define);
 
   if (!has('state')) instance.define.state = {};
   if (!has('nodes')) instance.define.nodes = [];
-  if (!has('id')) instance.define.id = identifier;
-  if (identifier !== instance.define.id) identifier = camelCase(instance.define.id);
+  if (!has('name')) instance.define.name = identifier;
+  if (identifier !== instance.define.name) identifier = camelCase(instance.define.name);
 
-  if (name !== original && /^[A-Z]|[_-]/.test(instance.define.id)) {
+  if (name !== original && /^[A-Z]|[_-]/.test(instance.define.name)) {
     log(LogType.WARN, [
-      `Component identifer name "${instance.define.id}" must use camelCase format.`,
+      `Component identifer name "${instance.define.name}" must use camelCase format.`,
       `The identifer has been converted to "${identifier}"`
     ]);
   }
@@ -67,8 +71,14 @@ export function registerComponents (components: { [id: string]: Register }, isVa
     const instance = components[id];
     const identifier = isValidID ? id : getComponentId(instance, id);
     if (!$registry.has(identifier)) {
+
       $registry.set(identifier, instance);
-      log(LogType.VERBOSE, `Component ${instance.name} registered using id: ${identifier}`, Colors.PINK);
+
+      log(
+        LogType.VERBOSE,
+        `Component ${instance.name} registered using id: ${identifier}`,
+        Colors.PINK
+      );
     }
   }
 
